5fda8c34f83e5d3bdee82c26998be797063b79af
HoangHieu <dinhhoanghieu321@gmail.com>
2022-03-20 22:03:16 +0700

    Create Main Screen and Food Detail Screen

---
+++ lib/pages/food/popular_food_detail.dart
@@ -0,0 +1,150 @@
+import 'package:flutter/material.dart';
+import 'package:food_delivery/utils/colors.dart';
+import 'package:food_delivery/utils/dimensions.dart';
+import 'package:food_delivery/widgets/app_column.dart';
+import 'package:food_delivery/widgets/app_icon.dart';
+import 'package:food_delivery/widgets/big_text.dart';
+import 'package:food_delivery/widgets/expandable_text_widget.dart';
+import 'package:food_delivery/widgets/icon_and_text_widget.dart';
+import 'package:food_delivery/widgets/small_text.dart';
+
+class PopularFoodDetail extends StatelessWidget {
+  const PopularFoodDetail({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Stack(
+        children: [
+          //Background image
+          Positioned(
+            left: 0,
+            right: 0,
+            child: Container(
+              width: double.maxFinite,
+              height: Dimensions.popularFoodImgSize,
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                  fit: BoxFit.cover,
+                  image: AssetImage('assets/image/food0.png'),
+                ),
+              ),
+            ),
+          ),
+          //Icon widgets
+          Positioned(
+            top: Dimensions.height45,
+            left: Dimensions.width20,
+            right: Dimensions.width20,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: const [
+                AppIcon(icon: Icons.arrow_back_ios),
+                AppIcon(icon: Icons.shopping_cart_outlined),
+              ],
+            ),
+          ),
+          //Text('Introduce') + Introduction of food with expandable text widget
+          Positioned(
+            left: 0,
+            right: 0,
+            bottom: 0,
+            top: Dimensions.popularFoodImgSize - 20,
+            child: Container(
+              padding: EdgeInsets.only(
+                  left: Dimensions.width20,
+                  right: Dimensions.width20,
+                  top: Dimensions.height20),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.only(
+                  topRight: Radius.circular(Dimensions.radius20),
+                  topLeft: Radius.circular(Dimensions.radius20),
+                ),
+                color: Colors.white,
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const AppColumn(text: 'Chinese Side'),
+                  SizedBox(height: Dimensions.height20),
+                  BigText(text: 'Introduce'),
+                  SizedBox(height: Dimensions.height20),
+                  const Expanded(
+                    child: SingleChildScrollView(
+                      child: ExpandableTextWidget(
+                          text:
+                              'Biryani là một món cơm trộn có nguồn gốc từ những người Hồi giáo ở tiểu lục địa Ấn Độ. Món ăn này nấu từ gạo (gạo hạt dài và rời) kết hợp với những gia vị đặc trưng của Ấn Độ trộn với thịt thường là thịt gà, thịt dê, thịt cừu, hải sản như tôm, cá, và đôi khi ở một số vùng thì thàn phần gồm trứng hoặc rau củ như khoai tây được thêm vào. Biryani là món ăn phổ biến trên khắp tiểu lục địa Ấn Độ, món này đã trở nên phổ biến ở Nam Ấn Độ, đặc biệt là ở Tamil Nadu, Andhra Pradesh và Telangana, món Biryani này cũng phổ biến ở các khu vực khác như Kurdistan ở Iraq. Từ Biryani là một từ có nguồn gốc từ ngôn ngữ Ba Tư, được sử dụng làm ngôn ngữ chính thức ở các vùng khác nhau của Ấn Độ thời trung cổ dưới các triều đại Hồi giáo. Một giả thuyết cho rằng nó có nguồn gốc từ birinj (برنج) là từ tiếng Ba Tư có nghĩa là gạo. Một giả thuyết khác cho rằng nó có nguồn gốc từ biryan hoặc beriyan (بریان), có nghĩa là "chiên" hoặc "thịt nướng".'),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+      bottomNavigationBar: Container(
+        height: Dimensions.bottomHeightBar,
+        padding: EdgeInsets.only(
+          top: Dimensions.height30,
+          bottom: Dimensions.height30,
+          left: Dimensions.width20,
+          right: Dimensions.width20,
+        ),
+        decoration: BoxDecoration(
+          color: AppColors.buttonBackgroundColor,
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(Dimensions.radius20 * 2),
+            topRight: Radius.circular(Dimensions.radius20 * 2),
+          ),
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Container(
+              padding: EdgeInsets.only(
+                top: Dimensions.height20,
+                bottom: Dimensions.height20,
+                left: Dimensions.width20,
+                right: Dimensions.width20,
+              ),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(Dimensions.radius20),
+                color: Colors.white,
+              ),
+              child: Row(children: [
+                Icon(
+                  Icons.remove,
+                  color: AppColors.signColor,
+                ),
+                SizedBox(width: Dimensions.width5),
+                BigText(text: '0'),
+                SizedBox(width: Dimensions.width5),
+                Icon(
+                  Icons.add,
+                  color: AppColors.signColor,
+                ),
+              ]),
+            ),
+            Container(
+              padding: EdgeInsets.only(
+                top: Dimensions.height20,
+                bottom: Dimensions.height20,
+                left: Dimensions.width20,
+                right: Dimensions.width20,
+              ),
+              child: BigText(
+                text: '\$10 | Add to cart',
+                color: Colors.white,
+              ),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(Dimensions.radius20),
+                color: AppColors.mainColor,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/utils/dimensions.dart
@@ -0,0 +1,53 @@
+import 'package:get/get.dart';
+
+class Dimensions {
+  static double screenHeight = Get.context!.height; //820.5714285714286
+  static double screenWidth = Get.context!.width; //411.42857142857144
+
+  static double pageView = screenHeight / 2.56;
+  static double pageViewContainer = screenHeight / 3.73;
+  static double pageViewTextContainer = screenHeight / 6.84;
+
+  //dynamic height padding and margin
+  static double height5 = screenHeight / 164.11;
+  static double height10 = screenHeight / 82.06;
+  static double height15 = screenHeight / 54.70;
+  static double height20 = screenHeight / 41.03;
+  static double height30 = screenHeight / 27.35;
+  static double height45 = screenHeight / 18.24;
+
+  //dynamic width padding and margin
+  static double width5 = screenHeight / 164.11;
+  static double width10 = screenHeight / 82.06;
+  static double width15 = screenHeight / 54.70;
+  static double width20 = screenHeight / 41.03;
+  static double width30 = screenHeight / 27.35;
+  static double width45 = screenHeight / 18.24;
+
+  //font size
+  static double font12 = screenHeight / 68.38;
+  static double font14 = screenHeight / 58.61;
+  static double font16 = screenHeight / 51.29;
+  static double font20 = screenHeight / 41.03;
+  static double font26 = screenHeight / 31.56;
+
+  //radius
+  static double radius5 = screenHeight / 164.11;
+  static double radius15 = screenHeight / 54.70;
+  static double radius20 = screenHeight / 41.03;
+  static double radius30 = screenHeight / 27.35;
+
+  //icon size
+  static double iconSize16 = screenHeight / 51.29;
+  static double iconSize24 = screenHeight / 34.19;
+
+  //list view size
+  static double listViewImgSize = screenWidth / 3.43;
+  static double listViewTextContSize = screenWidth / 4.11;
+
+  //popular food
+  static double popularFoodImgSize = screenHeight / 2.34;
+
+  //bottom height
+  static double bottomHeightBar = screenHeight / 6.84;
+}
